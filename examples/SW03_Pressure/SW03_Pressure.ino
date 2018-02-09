3d72d58ba492c0517dcb9f3d295132295e4bb642
brendanvanbreda <brendanvanbreda@users.noreply.github.com>
2018-02-09 13:16:05 +0200

    Update SW03_Pressure.ino

--- examples/SW03_Pressure/SW03_Pressure.ino
+++ examples/SW03_Pressure/SW03_Pressure.ino
@@ -13,10 +13,6 @@
 	
 	The sensor communicates over the I2C Bus.
 	
-	------------------------TIPS--------------------------
-	Comment out this line ----->Wire.pins(2, 14); for support
-	on other devices
-	
 *************************************************************/
 
 #include <xCore.h>
@@ -24,12 +20,17 @@
 
 const int DELAY_TIME = 1000;
 
+xSW03 SW03;
+
 void setup(){
 	// Start the Serial Monitor
 	Serial.begin(115200);
 	
 	// Set the I2C Pins for CW01
-	Wire.pins(2, 14);	
+	#ifdef ESP8266
+	  Wire.pins(2, 14);
+	  Wire.setClockStretchLimit(15000);
+	#endif
 	
 	// Start the I2C Comunication
 	Wire.begin();
